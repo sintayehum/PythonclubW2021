efade1e0fc1c0d83fb3c881bb0e95e6f91650b16
sintayehum <sintayehumotosa8@gmail.com>
2021-02-25 01:31:25 -0800

    Detail.commit

--- Club/admin.py
+++ Club/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from.models import Pythontype, Product,Club
 # Register your models here.
+admin.site.register(Pythontype)
+admin.site.register(Product)
+admin.site.register(Club)
