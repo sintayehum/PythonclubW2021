71d0c4cc8be8752849b7acba3cb4b877cd0bee4f
sintayehum <sintayehumotosa8@gmail.com>
2021-03-22 15:11:27 -0700

    final project command

--- Club/forms.py
+++ Club/forms.py
@@ -4,3 +4,8 @@ from .models PythonType, product, Club
 class productForm(forms.modelForm):
     model=product
     field='_all_'
+    
+class CreateUserForm(UserCreationForm):
+   class Meta:
+       model = User
+       fields = ['username' , 'email' , 'password1' , 'password2']  
